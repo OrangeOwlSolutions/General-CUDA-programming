da22c45ea19899cb01d1448e63c34b0930f7a2bd
Orange Owl <orangeowlsolutions@gmail.com>
2017-06-16 12:46:04 +0200

    Update ILP_Part_I.cu

--- ILP_Part_I.cu
+++ ILP_Part_I.cu
@@ -12,15 +12,15 @@
 /********************************************************/
 /* KERNEL0 - NO INSTRUCTION LEVEL PARALLELISM (ILP = 0) */
 /********************************************************/
-__global__ void kernel0(int * __restrict__ d_a, const int * __restrict__ d_b, const int * __restrict__ d_c, const int N) {
+__global__ void kernel0(float * __restrict__ d_a, const float * __restrict__ d_b, const float * __restrict__ d_c, const int N) {
 
 	const int tid = threadIdx.x + blockIdx.x * blockDim.x;
 
 	if (tid < N) {
 
-		int a = d_a[tid];
-		int b = d_b[tid];
-		int c = d_c[tid];
+		float a = d_a[tid];
+		float b = d_b[tid];
+		float c = d_c[tid];
 
 		for (unsigned int i = 0; i < N_ITERATIONS; i++) {
 			a = a * b + c;
@@ -34,19 +34,19 @@ __global__ void kernel0(int * __restrict__ d_a, const int * __restrict__ d_b, co
 /*****************************************************/
 /* KERNEL1 - INSTRUCTION LEVEL PARALLELISM (ILP = 2) */
 /*****************************************************/
-__global__ void kernel1(int * __restrict__ d_a, const int * __restrict__ d_b, const int * __restrict__ d_c, const int N) {
+__global__ void kernel1(float * __restrict__ d_a, const float * __restrict__ d_b, const float * __restrict__ d_c, const int N) {
 
 	const int tid = threadIdx.x + blockIdx.x * blockDim.x;
 
 	if (tid < N / 2) {
 
-		int a1 = d_a[tid];
-		int b1 = d_b[tid];
-		int c1 = d_c[tid];
+		float a1 = d_a[tid];
+		float b1 = d_b[tid];
+		float c1 = d_c[tid];
 
-		int a2 = d_a[tid + N / 2];
-		int b2 = d_b[tid + N / 2];
-		int c2 = d_c[tid + N / 2];
+		float a2 = d_a[tid + N / 2];
+		float b2 = d_b[tid + N / 2];
+		float c2 = d_c[tid + N / 2];
 
 		for (unsigned int i = 0; i < N_ITERATIONS; i++) {
 			a1 = a1 * b1 + c1;
@@ -62,27 +62,27 @@ __global__ void kernel1(int * __restrict__ d_a, const int * __restrict__ d_b, co
 /*****************************************************/
 /* KERNEL2 - INSTRUCTION LEVEL PARALLELISM (ILP = 4) */
 /*****************************************************/
-__global__ void kernel2(int * __restrict__ d_a, const int * __restrict__ d_b, const int * __restrict__ d_c, const int N) {
+__global__ void kernel2(float * __restrict__ d_a, const float * __restrict__ d_b, const float * __restrict__ d_c, const int N) {
 
 	const int tid = threadIdx.x + blockIdx.x * blockDim.x;
 
 	if (tid < N / 4) {
 
-		int a1 = d_a[tid];
-		int b1 = d_b[tid];
-		int c1 = d_c[tid];
+		float a1 = d_a[tid];
+		float b1 = d_b[tid];
+		float c1 = d_c[tid];
 
-		int a2 = d_a[tid + N / 4];
-		int b2 = d_b[tid + N / 4];
-		int c2 = d_c[tid + N / 4];
+		float a2 = d_a[tid + N / 4];
+		float b2 = d_b[tid + N / 4];
+		float c2 = d_c[tid + N / 4];
 
-		int a3 = d_a[tid + N / 2];
-		int b3 = d_b[tid + N / 2];
-		int c3 = d_c[tid + N / 2];
+		float a3 = d_a[tid + N / 2];
+		float b3 = d_b[tid + N / 2];
+		float c3 = d_c[tid + N / 2];
 
-		int a4 = d_a[tid + 3 * N / 4];
-		int b4 = d_b[tid + 3 * N / 4];
-		int c4 = d_c[tid + 3 * N / 4];
+		float a4 = d_a[tid + 3 * N / 4];
+		float b4 = d_b[tid + 3 * N / 4];
+		float c4 = d_c[tid + 3 * N / 4];
 
 		for (unsigned int i = 0; i < N_ITERATIONS; i++) {
 			a1 = a1 * b1 + c1;
@@ -104,33 +104,33 @@ __global__ void kernel2(int * __restrict__ d_a, const int * __restrict__ d_b, co
 /********/
 int main() {
 
-	const int N = 8192 * 4;
+	const int N = 8192 * 64;
 
 	TimingGPU timerGPU;
 
-	int *h_a = (int*)malloc(N*sizeof(int));
-	int *h_a_result_host = (int*)malloc(N*sizeof(int));
-	int *h_a_result_device = (int*)malloc(N*sizeof(int));
-	int *h_b = (int*)malloc(N*sizeof(int));
-	int *h_c = (int*)malloc(N*sizeof(int));
+	float *h_a = (float*)malloc(N*sizeof(float));
+	float *h_a_result_host = (float*)malloc(N*sizeof(float));
+	float *h_a_result_device = (float*)malloc(N*sizeof(float));
+	float *h_b = (float*)malloc(N*sizeof(float));
+	float *h_c = (float*)malloc(N*sizeof(float));
 
 	for (int i = 0; i<N; i++) {
-		h_a[i] = 2;
-		h_b[i] = 1;
-		h_c[i] = 2;
+		h_a[i] = 2.;
+		h_b[i] = 1.;
+		h_c[i] = 2.;
 		h_a_result_host[i] = h_a[i];
 		for (unsigned int k = 0; k < N_ITERATIONS; k++) {
 			h_a_result_host[i] = h_a_result_host[i] * h_b[i] + h_c[i];
 		}
 	}
 
-	int *d_a; gpuErrchk(cudaMalloc((void**)&d_a, N*sizeof(int)));
-	int *d_b; gpuErrchk(cudaMalloc((void**)&d_b, N*sizeof(int)));
-	int *d_c; gpuErrchk(cudaMalloc((void**)&d_c, N*sizeof(int)));
+	float *d_a; gpuErrchk(cudaMalloc((void**)&d_a, N*sizeof(float)));
+	float *d_b; gpuErrchk(cudaMalloc((void**)&d_b, N*sizeof(float)));
+	float *d_c; gpuErrchk(cudaMalloc((void**)&d_c, N*sizeof(float)));
 
-	gpuErrchk(cudaMemcpy(d_a, h_a, N*sizeof(int), cudaMemcpyHostToDevice));
-	gpuErrchk(cudaMemcpy(d_b, h_b, N*sizeof(int), cudaMemcpyHostToDevice));
-	gpuErrchk(cudaMemcpy(d_c, h_c, N*sizeof(int), cudaMemcpyHostToDevice));
+	gpuErrchk(cudaMemcpy(d_a, h_a, N*sizeof(float), cudaMemcpyHostToDevice));
+	gpuErrchk(cudaMemcpy(d_b, h_b, N*sizeof(float), cudaMemcpyHostToDevice));
+	gpuErrchk(cudaMemcpy(d_c, h_c, N*sizeof(float), cudaMemcpyHostToDevice));
 
 	/***********/
 	/* KERNEL0 */
@@ -143,13 +143,13 @@ int main() {
 #endif
 	// --- Remember: timing is in ms
 	printf("GFlops = %f\n", (1.e-6)*((float)N*(float)N_ITERATIONS) / timerGPU.GetCounter());
-	gpuErrchk(cudaMemcpy(h_a_result_device, d_a, N*sizeof(int), cudaMemcpyDeviceToHost));
-	for (int i = 0; i<N; i++) if (h_a_result_device[i] != h_a_result_host[i]) { printf("Error at i=%i! Host = %i; Device = %i\n", i, h_a_result_host[i], h_a_result_device[i]); return 1; }
+	gpuErrchk(cudaMemcpy(h_a_result_device, d_a, N*sizeof(float), cudaMemcpyDeviceToHost));
+	for (int i = 0; i<N; i++) if (h_a_result_device[i] != h_a_result_host[i]) { printf("Error at i=%i! Host = %f; Device = %f\n", i, h_a_result_host[i], h_a_result_device[i]); return 1; }
 
 	/***********/
 	/* KERNEL1 */
 	/***********/
-	gpuErrchk(cudaMemcpy(d_a, h_a, N*sizeof(int), cudaMemcpyHostToDevice));
+	gpuErrchk(cudaMemcpy(d_a, h_a, N*sizeof(float), cudaMemcpyHostToDevice));
 	timerGPU.StartCounter();
 	kernel1 << <iDivUp(N / 2, BLOCKSIZE), BLOCKSIZE >> >(d_a, d_b, d_c, N);
 #ifdef DEBUG
@@ -158,13 +158,13 @@ int main() {
 #endif
 	// --- Remember: timing is in ms
 	printf("GFlops = %f\n", (1.e-6)*((float)N*(float)N_ITERATIONS) / timerGPU.GetCounter());
-	gpuErrchk(cudaMemcpy(h_a_result_device, d_a, N*sizeof(int), cudaMemcpyDeviceToHost));
-	for (int i = 0; i<N; i++) if (h_a_result_device[i] != h_a_result_host[i]) { printf("Error at i=%i! Host = %i; Device = %i\n", i, h_a_result_host[i], h_a_result_device[i]); return 1; }
+	gpuErrchk(cudaMemcpy(h_a_result_device, d_a, N*sizeof(float), cudaMemcpyDeviceToHost));
+	for (int i = 0; i<N; i++) if (h_a_result_device[i] != h_a_result_host[i]) { printf("Error at i=%i! Host = %f; Device = %f\n", i, h_a_result_host[i], h_a_result_device[i]); return 1; }
 
 	/***********/
 	/* KERNEL2 */
 	/***********/
-	gpuErrchk(cudaMemcpy(d_a, h_a, N*sizeof(int), cudaMemcpyHostToDevice));
+	gpuErrchk(cudaMemcpy(d_a, h_a, N*sizeof(float), cudaMemcpyHostToDevice));
 	timerGPU.StartCounter();
 	kernel2 << <iDivUp(N / 4, BLOCKSIZE), BLOCKSIZE >> >(d_a, d_b, d_c, N);
 #ifdef DEBUG
@@ -173,8 +173,8 @@ int main() {
 #endif
 	// --- Remember: timing is in ms
 	printf("GFlops = %f\n", (1.e-6)*(float)((float)N*(float)N_ITERATIONS) / timerGPU.GetCounter());
-	gpuErrchk(cudaMemcpy(h_a_result_device, d_a, N*sizeof(int), cudaMemcpyDeviceToHost));
-	for (int i = 0; i<N; i++) if (h_a_result_device[i] != h_a_result_host[i]) { printf("Error at i=%i! Host = %i; Device = %i\n", i, h_a_result_host[i], h_a_result_device[i]); return 1; }
+	gpuErrchk(cudaMemcpy(h_a_result_device, d_a, N*sizeof(float), cudaMemcpyDeviceToHost));
+	for (int i = 0; i<N; i++) if (h_a_result_device[i] != h_a_result_host[i]) { printf("Error at i=%i! Host = %f; Device = %f\n", i, h_a_result_host[i], h_a_result_device[i]); return 1; }
 
 	return 0;
 
